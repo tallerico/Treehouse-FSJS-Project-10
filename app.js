fee759b9b81a3df0e1d01e35fdf027a9ea6f6adf
Mike Tallerico <mtallerico1@gmail.com>
2018-11-07 08:04:24 -0500

    adding second file for books route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
+const booksRouter = require('./routes/books')
 
 var app = express()
 
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static('public'))
 
-app.use('/', indexRouter)
+app.use('/', indexRouter, booksRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
