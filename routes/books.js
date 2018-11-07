9badb5bc46736d780e4ab83c0f45d191b745bf3a
Mike Tallerico <mtallerico1@gmail.com>
2018-11-07 08:05:43 -0500

    buildes routes for books queries

---
+++ routes/books.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const { books } = require('../models')
+const { patrons } = require('../models')
+const { loans } = require('../models')
+
+module.exports = router
