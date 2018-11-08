713f3891ab52c6327e77b52e16238d11733b4b20
Mike Tallerico <mtallerico1@gmail.com>
2018-11-08 06:51:02 -0500

    too many changes to list

--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,47 @@ const { books } = require('../models')
 const { patrons } = require('../models')
 const { loans } = require('../models')
 
+router.get('/overdue_books', (req, res, next) => {
+	const mainDate = new Date()
+	const date = JSON.stringify(mainDate)
+	const currDate = date.split('T')[0]
+	console.log(currDate)
+	books
+		.findAll({
+			include: {
+				model: loans,
+				where: {
+					return_by: {
+						[Op.lt]: mainDate,
+					},
+					returned_on: null,
+				},
+			},
+		})
+		.then(books => {
+			console.log(books)
+			res.render('overdue_books', { books })
+		})
+})
+
+router.get('/checked_books', (req, res, next) => {
+	const mainDate = new Date()
+	const date = JSON.stringify(mainDate)
+	const currDate = date.split('T')[0]
+	console.log(currDate)
+	books
+		.findAll({
+			include: {
+				model: loans,
+				where: {
+					returned_on: null,
+				},
+			},
+		})
+		.then(books => {
+			console.log(books)
+			res.render('checked_books', { books })
+		})
+})
+
 module.exports = router
